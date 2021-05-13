3698626858c7e7f116e4a4a580b8707774c7f865
VishalVyavahare <vishalvtest@gmail.com>
2021-05-13 12:30:37 +0530

    changes for address and name db mappings

--- src/main/java/com/viswekshal/atheneum/model/Address.java
+++ src/main/java/com/viswekshal/atheneum/model/Address.java
@@ -1,6 +1,19 @@
 package com.viswekshal.atheneum.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "table_address")
 public class Address {
+	@Id
+	@Column(name = "id")
 	private String id;
 	private String addressLine1;
 	private String addressLine2;
@@ -8,5 +21,7 @@ public class Address {
 	private String city;
 	private String state;
 	private String country;
+	@OneToOne(mappedBy = "address")
+	private AuthorName author;
 
 }
