ee071e7f3c1b33dd2516315a8002a49b6e0796d3
VishalVyavahare <vishalvtest@gmail.com>
2021-05-13 17:45:08 +0530

    Banner changes

---
+++ src/main/java/com/viswekshal/atheneum/CustomBanner.java
@@ -0,0 +1,40 @@
+package com.viswekshal.atheneum;
+import java.io.PrintStream;
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+public class CustomBanner implements Banner {
+	@Override
+	public void printBanner(Environment arg0, Class<?> arg1, PrintStream arg2) {
+		
+		arg2.println("\n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                          tttt         hhhhhhh                                                                                                                                  \n" + 
+				"                       ttt:::t         h:::::h                                                                                                                                  \n" + 
+				"                       t:::::t         h:::::h                                                                                                                                  \n" + 
+				"                       t:::::t         h:::::h                                                                                                                                  \n" + 
+				"  aaaaaaaaaaaaa  ttttttt:::::ttttttt    h::::h hhhhh           eeeeeeeeeeee    nnnn  nnnnnnnn      aaaaaaaaaaaaa      eeeeeeeeeeee    uuuuuu    uuuuuu     mmmmmmm    mmmmmmm   \n" + 
+				"  a::::::::::::a t:::::::::::::::::t    h::::hh:::::hhh      ee::::::::::::ee  n:::nn::::::::nn    a::::::::::::a   ee::::::::::::ee  u::::u    u::::u   mm:::::::m  m:::::::mm \n" + 
+				"  aaaaaaaaa:::::at:::::::::::::::::t    h::::::::::::::hh   e::::::eeeee:::::een::::::::::::::nn   aaaaaaaaa:::::a e::::::eeeee:::::eeu::::u    u::::u  m::::::::::mm::::::::::m\n" + 
+				"           a::::atttttt:::::::tttttt    h:::::::hhh::::::h e::::::e     e:::::enn:::::::::::::::n           a::::ae::::::e     e:::::eu::::u    u::::u  m::::::::::::::::::::::m\n" + 
+				"    aaaaaaa:::::a      t:::::t          h::::::h   h::::::he:::::::eeeee::::::e  n:::::nnnn:::::n    aaaaaaa:::::ae:::::::eeeee::::::eu::::u    u::::u  m:::::mmm::::::mmm:::::m\n" + 
+				"  aa::::::::::::a      t:::::t          h:::::h     h:::::he:::::::::::::::::e   n::::n    n::::n  aa::::::::::::ae:::::::::::::::::e u::::u    u::::u  m::::m   m::::m   m::::m\n" + 
+				" a::::aaaa::::::a      t:::::t          h:::::h     h:::::he::::::eeeeeeeeeee    n::::n    n::::n a::::aaaa::::::ae::::::eeeeeeeeeee  u::::u    u::::u  m::::m   m::::m   m::::m\n" + 
+				"a::::a    a:::::a      t:::::t    tttttth:::::h     h:::::he:::::::e             n::::n    n::::na::::a    a:::::ae:::::::e           u:::::uuuu:::::u  m::::m   m::::m   m::::m\n" + 
+				"a::::a    a:::::a      t::::::tttt:::::th:::::h     h:::::he::::::::e            n::::n    n::::na::::a    a:::::ae::::::::e          u:::::::::::::::uum::::m   m::::m   m::::m\n" + 
+				"a:::::aaaa::::::a      tt::::::::::::::th:::::h     h:::::h e::::::::eeeeeeee    n::::n    n::::na:::::aaaa::::::a e::::::::eeeeeeee   u:::::::::::::::um::::m   m::::m   m::::m\n" + 
+				" a::::::::::aa:::a       tt:::::::::::tth:::::h     h:::::h  ee:::::::::::::e    n::::n    n::::n a::::::::::aa:::a ee:::::::::::::e    uu::::::::uu:::um::::m   m::::m   m::::m\n" + 
+				"  aaaaaaaaaa  aaaa         ttttttttttt  hhhhhhh     hhhhhhh    eeeeeeeeeeeeee    nnnnnn    nnnnnn  aaaaaaaaaa  aaaa   eeeeeeeeeeeeee      uuuuuuuu  uuuummmmmm   mmmmmm   mmmmmm\n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"                                                                                                                                                                                \n" + 
+				"");   
+                                                                                                                                                              
+                                                                                                                                                              
+
+	}
+}
